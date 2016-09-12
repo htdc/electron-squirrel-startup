7bdb5ef18170cc798f2f52590c23a1c2429567c7
Hans Wannop <hanswannop@me.com>
2016-09-12 16:44:32 +1000

    Add startup shortcut

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ var check = function() {
     var target = path.basename(process.execPath);
 
     if (cmd === '--squirrel-install' || cmd === '--squirrel-updated') {
-      run(['--createShortcut=' + target + ''], app.quit);
+      run(["--createShortcut=" + target + " --shortcut-locations='Startup,Desktop,StartMenu'"], app.quit);
       return true;
     }
     if (cmd === '--squirrel-uninstall') {
-      run(['--removeShortcut=' + target + ''], app.quit);
+      run(["--removeShortcut=" + target + " --shortcut-locations='Startup,Desktop,StartMenu'"], app.quit);
       return true;
     }
     if (cmd === '--squirrel-obsolete') {
